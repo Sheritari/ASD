8a47467f1d281dd109f62dee5fb6454755a1a479
Sheritari <92041560+Sheritari@users.noreply.github.com>
2021-11-10 11:44:58 +0800

    Update Main.java

--- Homework5/src/com/company/Main.java
+++ Homework5/src/com/company/Main.java
@@ -58,8 +58,20 @@ public class Main {
             sum += task.get();
         }
 
-        // Ожидание завершения запущенных задач
-        executor.shutdown();
+        // Ожидание завершения запущенных задач и остановка исполнителя
+        try {
+            System.out.println("attempt to shutdown executor");
+            executor.shutdown();
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.err.println("tasks interrupted");
+        } finally {
+            if (!executor.isTerminated()) {
+                System.err.println("cancel non-finished tasks");
+            }
+            executor.shutdownNow();
+            System.out.println("shutdown finished");
+        }
 
         System.out.println("Count of space: " + sum);
 
